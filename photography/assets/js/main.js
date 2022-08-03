b2f4864ada850047e0c2c9dcfed41717ee442d18
WeedMD-Support <support@weedmd.com>
2022-08-03 12:55:18 -0400

    Add photo

--- photography/assets/js/main.js
+++ photography/assets/js/main.js
@@ -272,7 +272,6 @@
                         data = exifDatas[$image_img.data('name')] = getExifDataMarkup(this);
                     });
                 }
-                console.log(data)
                 return data !== undefined ? '<p>' + data + '</p>' : ' ';
             },
             fadeSpeed: 300,
@@ -308,16 +307,44 @@
             });
 
         function getExifDataMarkup(img) {
-            var exif_display = $('#main').data('exif-display');
+            var exif = fetchExifData(img);
             var template = '';
-            for (var current in exif_display) {
-		var current_data = exif_display[current];
-                var exif = EXIF.getTag(img, current_data['tag']);
-                if (typeof exif !== "undefined") {
-                    template += '<i class="fa fa-' + current_data['icon'] +  '" aria-hidden="true"></i> ' + exif + '&nbsp;&nbsp;';
+            for (var info in exif) {
+                if (info === "model") {
+                    template += '<i class="fa fa-camera-retro" aria-hidden="true"></i> ' + exif["model"] + '&nbsp;&nbsp;';
+                }
+                if (info === "aperture") {
+                    template += '<i class="fa fa-dot-circle-o" aria-hidden="true"></i> f/' + exif["aperture"] + '&nbsp;&nbsp;';
+                }
+                if (info === "shutter_speed") {
+                    template += '<i class="fa fa-clock-o" aria-hidden="true"></i> ' + exif["shutter_speed"] + '&nbsp;&nbsp;';
+                }
+                if (info === "iso") {
+                    template += '<i class="fa fa-info-circle" aria-hidden="true"></i> ' + exif["iso"] + '&nbsp;&nbsp;';
                 }
             }
             return template;
+        }
+
+        function fetchExifData(img) {
+            var exifData = {};
+
+            if (EXIF.getTag(img, "Model") !== undefined) {
+                exifData.model = EXIF.getTag(img, "Model");
+            }
+
+            if (EXIF.getTag(img, "FNumber") !== undefined) {
+                exifData.aperture = EXIF.getTag(img, "FNumber");
+            }
+
+            if (EXIF.getTag(img, "ExposureTime") !== undefined) {
+                exifData.shutter_speed = EXIF.getTag(img, "ExposureTime");
+            }
+
+            if (EXIF.getTag(img, "ISOSpeedRatings") !== undefined) {
+                exifData.iso = EXIF.getTag(img, "ISOSpeedRatings");
+            }
+            return exifData;
         }
 
     });
